9525d1e1aa9855989e4bee04de647a62a5f9a689
pilchard <pilchard@imac>
2013-02-16 21:29:41 -0800

    Google Places autocomplete provisionally in place.

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -9,7 +9,96 @@
   <meta name="robots" content="index, follow" />
 
   <?php echo css('assets/styles/styles.css') ?>
+  
+  <style>
+        body {
+          font-family: sans-serif;
+          font-size: 14px;
+        }
+        #map_canvas {
+          height: 400px;
+          width: 600px;
+          margin-top: 0.6em;
+        }
+        input {
+          border: 1px solid  rgba(0, 0, 0, 0.5);
+        }
+        input.notfound {
+          border: 2px solid  rgba(255, 0, 0, 0.4);
+        }
+  </style>
+  
+  <script type="text/javascript"
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCeEfSiOnA5tVJgUrf_IUSpB1LGmADooew&sensor=false&libraries=places">
+      </script>
+  
+  <script>
+        function initialize() {
+          var mapOptions = {
+            center: new google.maps.LatLng(-33.8688, 151.2195),
+            zoom: 13,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+          };
+          var map = new google.maps.Map(document.getElementById('map_canvas'),
+            mapOptions);
 
+            var autocompleteOptions = {
+              types: ['(cities)']
+             };
+
+             var input = document.getElementById('searchTextField');
+             var autocomplete = new google.maps.places.Autocomplete(input, autocompleteOptions);
+
+          var infowindow = new google.maps.InfoWindow();
+          var marker = new google.maps.Marker({
+            map: map
+          });
+
+          google.maps.event.addListener(autocomplete, 'place_changed', function() {
+            infowindow.close();
+            marker.setVisible(false);
+            input.className = '';
+            var place = autocomplete.getPlace();
+            if (!place.geometry) {
+              // Inform the user that the place was not found and return.
+              input.className = 'notfound';
+              return;
+            }
+            console.log(place.geometry.location);
+            // If the place has a geometry, then present it on a map.
+            if (place.geometry.viewport) {
+              map.fitBounds(place.geometry.viewport);
+            } else {
+              map.setCenter(place.geometry.location);
+              map.setZoom(17);  // Why 17? Because it looks good.
+            }
+            var image = {
+              url: place.icon,
+              size: new google.maps.Size(71, 71),
+              origin: new google.maps.Point(0, 0),
+              anchor: new google.maps.Point(17, 34),
+              scaledSize: new google.maps.Size(35, 35)
+            };
+            marker.setIcon(image);
+            marker.setPosition(place.geometry.location);
+            marker.setVisible(true);
+
+            var address = '';
+            if (place.address_components) {
+              address = [
+                (place.address_components[0] && place.address_components[0].short_name || ''),
+                (place.address_components[1] && place.address_components[1].short_name || ''),
+                (place.address_components[2] && place.address_components[2].short_name || '')
+              ].join(' ');
+            }
+
+            infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+            infowindow.open(map, marker);
+          });
+        }
+        google.maps.event.addDomListener(window, 'load', initialize);
+  </script>
+  
 </head>
 
 <body>
